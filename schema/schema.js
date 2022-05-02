37f1aa6e7b7771ef8ce152bb56623ff5805c9b93
Kate Medvedeva <katemed@Kates-MacBook-Pro.local>
2022-05-02 17:29:19 -0400

    created mutations for adding and removing

--- schema/schema.js
+++ schema/schema.js
@@ -98,6 +98,56 @@ const DirectorType = new GraphQLObjectType({
     }),
 });
 
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addDirector: {
+            type: DirectorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt },
+            },
+            resolve(parent, args) {
+                const director = new Directors({
+                    name: args.name,
+                    age: args.age,
+                });
+                return director.save();
+            },
+        },
+        addMovie: {
+            type: MovieType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                directorId: { type: GraphQLID },
+            },
+            resolve(parent, args) {
+                const movie = new Movies({
+                    name: args.name,
+                    genre: args.genre,
+                    directorId: args.directorId,
+                });
+                return movie.save();
+            },
+        },
+        deleteDirector: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Directors.findByIdAndRemove(args.id);
+            },
+        },
+        deleteMovie: {
+            type: MovieType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Movies.findByIdAndRemove(args.id);
+            },
+        },
+    },
+});
+
 const Query = new GraphQLObjectType({
     name: "Query",
     fields: {
@@ -136,4 +186,5 @@ const Query = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
     query: Query,
+    mutation: Mutation,
 });
