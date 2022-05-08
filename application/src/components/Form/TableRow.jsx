d308adf8bb14db11298e664ae4c7a23691604daf
KateM3d <ekaterina8medvedeva@gmail.com>
2022-05-08 12:50:05 -0400

    added TableRow edit option

--- application/src/components/Form/TableRow.jsx
+++ application/src/components/Form/TableRow.jsx
@@ -1,7 +1,44 @@
 import { useState } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import Modal from "../Modal/Modal";
+import { useQuery, gql } from "@apollo/client";
 import "./Forms";
+
+const MOVIES = gql`
+  query Movies {
+    movie {
+      id
+      name
+    }
+  }
+`;
+
+const Movies = () => {
+  const { loading, error, data } = useQuery(MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return data.map(({ id, movie }) => (
+    <div key={id}>
+      <>
+        <p className="tableRow-name">{movie.name}</p>
+        <p className="tableRow-name">{movie.genre}</p>
+        <p className="tableRow-name">rate 1</p>
+        <p className="tableRow-name">{movie.director}</p>
+        <p className="tableRow-name">watched</p>
+        <div className="btn-container">
+          {/* <button className="icon" onClick={() => handleEdit()}>
+            <AiFillEdit />
+          </button>
+          <button className="icon" onClick={() => handleDelete()}>
+            <AiFillDelete />
+          </button> */}
+        </div>
+      </>
+    </div>
+  ));
+};
 
 const TableRow = ({ selected }) => {
   const [edit, setEdit] = useState(false);
@@ -22,21 +59,23 @@ const TableRow = ({ selected }) => {
         <>
           <div className="tableRow">
             {selected === "movies" ? (
-              <>
-                <p className="tableRow-name">movie1</p>
-                <p className="tableRow-name">genre1</p>
-                <p className="tableRow-name">rate 1</p>
-                <p className="tableRow-name">director1</p>
-                <p className="tableRow-name">watched</p>
-                <div className="btn-container">
-                  <button className="icon" onClick={() => handleEdit()}>
-                    <AiFillEdit />
-                  </button>
-                  <button className="icon" onClick={() => handleDelete()}>
-                    <AiFillDelete />
-                  </button>
-                </div>
-              </>
+              // <>
+              //   <p className="tableRow-name">movie1</p>
+              //   <p className="tableRow-name">genre1</p>
+              //   <p className="tableRow-name">rate 1</p>
+              //   <p className="tableRow-name">director1</p>
+              //   <p className="tableRow-name">watched</p>
+              //   <div className="btn-container">
+              //     <button className="icon" onClick={() => handleEdit()}>
+              //       <AiFillEdit />
+              //     </button>
+              //     <button className="icon" onClick={() => handleDelete()}>
+              //       <AiFillDelete />
+              //     </button>
+              //   </div>
+              //
+              // </>
+              <Movies />
             ) : (
               <>
                 <p className="tableRow-name">name1</p>
--- application/src/index.jsx
+++ application/src/index.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3005",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
