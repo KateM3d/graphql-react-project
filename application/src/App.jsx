0374c859bdab6461a67961326785240fe970ae0e
KateM3d <ekaterina8medvedeva@gmail.com>
2022-05-12 22:32:07 -0400

    fixed ApolloProvider

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,8 +1,24 @@
 import "./App.scss";
 import Tabs from "./components/Tabs/Tabs";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function App() {
-  return <Tabs />;
+  const client = new ApolloClient({
+    uri: "http://localhost:3005/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Tabs />
+    </ApolloProvider>
+  );
 }
 
 export default App;
